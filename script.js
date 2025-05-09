571c8cba99f559e31eefbefa005a74fe4f652ce6
GomesGuilhermePT07 <gomesguilherme.pt@gmail.com>
2025-05-09 11:41:02 +0100

    jump function done

--- script.js
+++ script.js
@@ -11,3 +11,23 @@ document.addEventListener("keydown", function(e) {
         if (!isJumping) jump();
     }
 });
+
+function jump() {
+    isJumping = true;
+    let jumpHeight = 0;
+    let upInterval = setInterval(() => {
+        if (jumpHeight >= 100) {
+            clearInterval(upInterval);
+            let downInterval = setInterval(() => {
+                if (jumpHeight <= 0) {
+                    clearInterval(downInterval);
+                    isJumping = false;
+                }
+                jumpHeight -= 5;
+                player.style.bottom = jumpHeight + "px";
+            }, 20);
+        }
+        jumpHeight += 5;
+        player.style.bottom = jumpHeight + "px";
+    }, 20);
+}
